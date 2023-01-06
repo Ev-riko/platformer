c423ef5eb9b2849c541c250860c684be531e87b0
LUF-FY <margiev.erik@list.ru>
2023-01-06 21:53:27 +0300

    Update Level

--- Assets/PIxelCrew/FollowTarget.cs
+++ Assets/PIxelCrew/FollowTarget.cs
@@ -7,11 +7,12 @@ namespace PixelCrew
     public class FollowTarget : MonoBehaviour
     {
         [SerializeField] private Transform _target;
+        [SerializeField] private float _damping;
 
         private void LateUpdate()
         {
-            var detination = new Vector3(_target.position.x, _target.position.y, transform.position.z);
-            transform.position = detination;
+            var destination = new Vector3(_target.position.x, _target.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, destination, Time.deltaTime * _damping);
         }
     }
 }
