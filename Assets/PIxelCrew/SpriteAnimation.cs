d6be84c654cfcfbe5b12260b58ecba49c5a3f729
LUF-FY <margiev.erik@list.ru>
2023-08-01 16:41:15 +0300

    Update the SpriteAnimation Added the ability to create multiple clips

--- Assets/PIxelCrew/SpriteAnimation.cs
+++ Assets/PIxelCrew/SpriteAnimation.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -7,52 +8,101 @@ namespace PixelCrew
     [RequireComponent(typeof(SpriteRenderer))]
     public class SpriteAnimation : MonoBehaviour
     {
-        [SerializeField] private int _frameRate;
-        [SerializeField] private bool _loop;
-        [SerializeField] private Sprite[] _sprites;
-        [SerializeField] private bool _desableOnComplete;
-        [SerializeField] private UnityEvent _onComplete;
+        [Range(0, 30)][SerializeField] private int _frameRate = 10;
+        [SerializeField] private AnimationClip[] _clips;
+        [SerializeField] private UnityEvent<string> _onComplete;
 
         private SpriteRenderer _renderer;
         private float _secondsPerFrame;
-        private int _currentSpriteIndex;
-        private float _nextUpdateTime;
+        private int _currentFrame;
+        private float _nextFrameTime;
+
+        private int _currentClipIndex;
 
         private void Start()
         {
             _renderer = GetComponent<SpriteRenderer>();
+
+            _secondsPerFrame = 1f / _frameRate;
+
+            _currentClipIndex = 0;
+
+            StartAnimation();
         }
 
         private void OnEnable()
         {
-            _secondsPerFrame = 1f / _frameRate;
-            _nextUpdateTime = Time.time + _secondsPerFrame;
-            _currentSpriteIndex = 0;
+            _nextFrameTime = Time.time + _secondsPerFrame;
         }
 
         private void Update()
         {
-            if (_nextUpdateTime > Time.time) return;
+            if (_nextFrameTime > Time.time) return;
 
-            if (_currentSpriteIndex >= _sprites.Length)
+            var clip = _clips[_currentClipIndex];
+            if (_currentFrame >= clip.Sprites.Length)
             {
-                if (_loop)
+                if (clip.Loop)
                 {
-                    _currentSpriteIndex = 0;
+                    _currentFrame = 0;
                 }
                 else
                 {
-                    enabled = !_desableOnComplete;
-                    _onComplete?.Invoke();
+                    clip.OnComplete?.Invoke();
+                    _onComplete?.Invoke(clip.Name);
+                    enabled = clip.AllowNextClip;
+                    if (clip.AllowNextClip)
+                    {
+                        _currentFrame = 0;
+                        _currentClipIndex = (int)Mathf.Repeat(_currentClipIndex + 1, _clips.Length);
+                    }
                     return;
                 }
             }
 
-            _renderer.sprite = _sprites[_currentSpriteIndex];
-            _nextUpdateTime += _secondsPerFrame;
-            _currentSpriteIndex++;
-
+            _renderer.sprite = clip.Sprites[_currentFrame];
+            _nextFrameTime += _secondsPerFrame;
+            _currentFrame++;
+            //Debug.Log($"_currentSpriteIndex: {_currentFrame}");
         }
+
+        private void StartAnimation()
+        {
+            _nextFrameTime = Time.time + _secondsPerFrame;
+            _currentFrame = 0;
+        }
+
+        public void SetClip(string clipName)
+        {
+            for (int i = 0; i < _clips.Length; i++)
+            {
+                if (_clips[i].Name == clipName)
+                {
+                    _currentClipIndex = i;
+                    StartAnimation();
+                    return;
+                }
+            }
+            enabled = false;
+        }
+
+    }
+
+
+    [Serializable]
+    public class AnimationClip
+    {
+        [SerializeField] private string _name;
+        [SerializeField] private Sprite[] _sprites;
+        [SerializeField] private bool _loop;
+        [SerializeField] private bool _allowNextClip;
+        [SerializeField] private UnityEvent _onComplete;
+
+        public string Name => _name;
+        public Sprite[] Sprites => _sprites;
+        public bool Loop => _loop;
+        public bool AllowNextClip => _allowNextClip;
+        public UnityEvent OnComplete => _onComplete;
 
     }
 }
